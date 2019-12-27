5aada2226145a7e30e54c6dcbd10c5c4c1251291
thiagohb <thiagohb@gmail.com>
2019-12-27 00:19:57 -0300

    UnitTest improvement.

--- src/test/java/nl/rabobank/powerofattorney/service/AuthorizationServiceTest.java
+++ src/test/java/nl/rabobank/powerofattorney/service/AuthorizationServiceTest.java
@@ -40,15 +40,26 @@ class AuthorizationServiceTest {
         final Account acc1 = new Account(acc1Id, acc1Owner, acc1Balance, acc1Created, null);
         when(accountService.getAccount(acc1Id)).thenReturn(acc1);
 
+        final String acc2Id = "NL23RABO333333333";
+        final String acc2Owner = "Owner Three";
+        final long acc2Balance = 450L;
+        final String acc2Created = "24-05-2009";
+        final Account acc2 = new Account(acc2Id, acc2Owner, acc2Balance, acc2Created, null);
+        when(accountService.getAccount(acc2Id)).thenReturn(acc2);
+
         final String userGrantor = "testGrantor";
         final String userId = "testUser";
         final String direction = "GIVEN";
 
         final String poa1Id = "01";
         final String[] poa1Auths = {"DEBIT_CARD", "VIEW", "PAYMENT"};
+        final String poa3Id = "03";
+        final String[] poa3Auths = {"DEBIT_CARD", "VIEW", "PAYMENT"};
 
         final PowerOfAttorney poa1 = new PowerOfAttorney(poa1Id, userGrantor, userId, acc1Id, direction, poa1Auths, null);
-        final List<PowerOfAttorney> poas = Arrays.asList(poa1);
+        final PowerOfAttorney poa2 = new PowerOfAttorney("02", userGrantor, "anotherUser", "NL23RABO222222222", direction, poa1Auths, null);
+        final PowerOfAttorney poa3 = new PowerOfAttorney(poa3Id, userGrantor, userId, acc2Id, direction, poa3Auths, null);
+        final List<PowerOfAttorney> poas = Arrays.asList(poa1, poa2, poa3);
         when(powerOfAttorneyService.getPowerOfAttorneys()).thenReturn(poas);
 
         // when
@@ -58,7 +69,7 @@ class AuthorizationServiceTest {
         assertNotNull(authorizationInformation);
         assertEquals(userId, authorizationInformation.getUserId());
         assertNotNull(authorizationInformation.getAggregatedInformations());
-        assertEquals(1, authorizationInformation.getAggregatedInformations().size());
+        assertEquals(2, authorizationInformation.getAggregatedInformations().size());
         final AggregatedInformation aggregatedInfo1 = authorizationInformation.getAggregatedInformations().get(0);
         assertEquals(poa1Id, aggregatedInfo1.getId());
         assertEquals(userGrantor, aggregatedInfo1.getGrantor());
@@ -70,6 +81,18 @@ class AuthorizationServiceTest {
         assertEquals(acc1Balance, account1.getBalance());
         assertEquals(acc1Created, account1.getCreated());
         assertNull(account1.getEnded());
+
+        final AggregatedInformation aggregatedInfo2 = authorizationInformation.getAggregatedInformations().get(1);
+        assertEquals(poa3Id, aggregatedInfo2.getId());
+        assertEquals(userGrantor, aggregatedInfo2.getGrantor());
+        assertEquals(direction, aggregatedInfo2.getDirection());
+        final Account account2 = aggregatedInfo2.getAccount();
+        assertNotNull(account2);
+        assertEquals(acc2Id, account2.getId());
+        assertEquals(acc2Owner, account2.getOwner());
+        assertEquals(acc2Balance, account2.getBalance());
+        assertEquals(acc2Created, account2.getCreated());
+        assertNull(account2.getEnded());
     }
 
 }
